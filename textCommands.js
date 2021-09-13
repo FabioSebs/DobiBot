ff5a68e406c7a6614f5d304fc5a6dd7a4b3577ea
FabioSebs <sebsphotosofficial@gmail.com>
2021-09-13 09:17:48 -0400

    Added MP3s

--- textCommands.js
+++ textCommands.js
@@ -5,11 +5,13 @@ require('dotenv').config();  	//Dotenv Import
 const path = require('path')
 const audio1 = path.join(__dirname, 'DEONGODTOP.mp3')
 const audio2 = path.join(__dirname,'DEONDEFUSE.mp3')
+const audio3 = path.join(__dirname, 'DEONWEEB.mp3')
+const audio4 = path.join(__dirname, 'DARKKASUE1.mp3')
 let commandList;
 
 
 const listCommands = (msg) => {
-	[...commandList] = ['!dobi', 'dobiDefuse', '!dobiWelcome', '!dobiCommands']
+	[...commandList] = ['!dobi', '!dobiDefuse', '!dobiWelcome', '!dobiCommands']
 	if (msg.content === '!dobiCommands') {
 		commandList.forEach(i => {
 			msg.reply(i)
@@ -34,6 +36,28 @@ const dobiAudio = (msg) => {
 	    voiceChannel.join()
 	    .then((connection)=> {
 		    const dispatcher = connection.play(audio2)
+		    setTimeout(()=> {
+			    voiceChannel.leave()
+		    }, 4000)
+	    })
+	    .catch(e=>console.log(e))
+    }
+
+	if (msg.content === '!dobiWeeb') {
+	    voiceChannel.join()
+	    .then((connection)=> {
+		    const dispatcher = connection.play(audio3)
+		    setTimeout(()=> {
+			    voiceChannel.leave()
+		    }, 17500)
+	    })
+	    .catch(e=>console.log(e))
+    }
+
+	if (msg.content === '!dobiKasue') {
+	    voiceChannel.join()
+	    .then((connection)=> {
+		    const dispatcher = connection.play(audio4)
 		    setTimeout(()=> {
 			    voiceChannel.leave()
 		    }, 5000)
