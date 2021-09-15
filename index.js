fc3ee9d3cea217ece23c72e3a78b4211db2c1c64
FabioSebs <sebsphotosofficial@gmail.com>
2021-09-15 07:41:16 -0400

    Valorant API

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ require('dotenv').config();  	//Dotenv Import
 const path = require('path')
 const audio = path.join(__dirname, 'DEONGODTOP.mp3')
 const textCommands = require('./textCommands')
-const { RiotValorantApi, Regions} = require("riot-valorant-api");
+const { RiotValorantApi, Regions, Locales, Queue } = require("riot-valorant-api");
+const axios = require('axios');
 
-const valorantAPI = new RiotValorantApi(process.env.VALO_TOKEN)
+const valorantAPI = new RiotValorantApi(process.env.VALO_TOKEN, Regions.NORTH_AMERICA)
 
 // Once DobiBot Connects
 client.on("ready",()=>{
-	console.log(`Logged in as ${client.user.tag}`)
+ console.log(`Logged in as ${client.user.tag}`)
 })
 
 //Handles any message commands
@@ -30,6 +31,24 @@ client.on("message", msg => {
 	// msg.content === '!leave' ? voiceChannel.leave() : null
 })
 
+//VALORANT CONTENT
+const showValoData = async () => {
+	const content = await valorantAPI.ContentV1.getAllContent(Locales.EN_US)
+	console.log(content)
+};
+
+//GET LEADERBOARDS
+const showLeaderboard = async () => {
+	try {
+		const leaderboard = await valorantAPI.RankedV1.getLeaderboardByAct("97b39124-46ce-8b55-8fd1-7cbf7ffe173f", size=20, startIndex = 0)
+		console.log(leaderboard)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+// showValoData()
+showLeaderboard()
 
 // Logs DobiBot in
 client.login(process.env.TOKEN)
